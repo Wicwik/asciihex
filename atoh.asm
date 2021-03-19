ba6f2e44f69f84b350674b3408344a438009eeb4
Robert Belanec <belanecrobert22@gmail.com>
2021-03-19 13:24:43 +0100

    added comments

--- atoh.asm
+++ atoh.asm
@@ -1,4 +1,4 @@
-TITLE asci to hex module
+TITLE Asci to hex module
 	.MODEL small
 
 	.STACK 100h
@@ -9,23 +9,26 @@ TITLE asci to hex module
 
 	.CODE
 
-		
+; buffer and hexstr is from another file
+extrn buffer: byte
+extrn hexstr: byte
+
+; make artefacts public, allowing them to be accesed from other files
 public ascii_to_hex
 
-extrn buffer:BYTE
-extrn hexstr:BYTE
-
+; convers ascii char to hexadecimal number
+; and store it to hexstring
 ascii_to_hex:
-	mov bx, offset hexchars
-	mov al, [buffer]
-	mov ah, al
-	and al, 00001111b
-	xlat
-
-	mov [hexstr + 1], al
-	shr ax, 12
-	xlat
-	mov hexstr, al
+	mov bx, offset hexchars			; set bx to begining of hexchars string
+	mov al, [buffer] 				; set al, to address of buffer
+	mov ah, al 						; ah == al
+	and al, 00001111b 				; zero out the top 4 bits
+	xlat 							; set al to next char DS:[BX + AL]
+	mov [hexstr + 1], al 			; set hexstr to al value
+	
+	shr ax, 12 						; shift right for the upper 4 bits
+	xlat 							; set al to next char DS:[BX + AL]
+	mov hexstr, al 					; set hexstr to al value
 
 	ret
 
